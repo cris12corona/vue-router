c577a9a1dce340e6dba87b835d9050f71d6b2b93
Christopher <cris12corona@gmail.com>
2024-01-30 10:02:34 -0600

    Prograna de pokemones

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <nav class="navbar bg-body-tertiary">
+  <div class="container">
+    <router-link class="navbar-brand" to="/">
+      <img src="@/assets/logo.svg"      alt="Bootstrap" width="30" height="24">
+      PokeApi
+    </router-link>
+    <div>
+      <router-link active-class="active" class="btn btn-outline-primary me-2" to="/">Home</router-link>
+      <router-link active-class="active" class="btn btn-outline-primary" to="/pokemon">Pokemones</router-link>
+    </div>
+  </div>
+</nav>
+
+  <!--<header>
+    <img alt="Vue logo" 
+    class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/pokemon">Pokemon</RouterLink>
+      </nav>
+    </div>
+  </header>-->
+  
+<div class="container">
+  <RouterView />
+</div>
+  
+</template>
+
+
---
+++ src/views/PokemonView.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+import axios from 'axios';
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
+
+const pokemons = ref([])
+
+const getData = async () => {
+try {
+    const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon"); 
+    console.log(data.results);
+    pokemons.value = data.results
+} catch (error) {
+   console.log(error);
+}
+
+};
+
+getData();
+</script>
+
+<template>
+ <h1>Pokemones</h1>   
+ <ul>
+    <li v-for="(poke, index) in pokemons" :key="index">  
+      <router-link :to="`/poke/${poke.name}`">
+        {{ poke.name }}
+    </router-link>   
+    
+    </li>
+ </ul>
+</template>
